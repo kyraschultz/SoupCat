51efe6a1f58766c6551ccbff8b060b41a25a5727
kyraschultz <KDS85@pitt.edu>
2023-11-29 21:33:46 -0500

    Player movement & Collision

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerControls controls;
+    Vector2 direction = new Vector2(0f, 0f);
+
+    public Rigidbody2D playerRB;
+    public float speed = 400;
+    bool isFacingRight = true;
+    public Animator animator;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        controls.Walk.Move.performed += ctx =>
+        {
+            direction = ctx.ReadValue<Vector2>();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.velocity = new Vector2(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime);
+        animator.SetFloat("Speed", Mathf.Abs(direction.x + direction.y));
+
+        if (isFacingRight && direction.x < 0 || !isFacingRight && direction.x >0)
+            Flip();
+
+    
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+}
