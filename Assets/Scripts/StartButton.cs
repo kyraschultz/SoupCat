53cd59d2f2c7d61c784f6a427ce26c166c83417e
kyraschultz <KDS85@pitt.edu>
2023-11-30 19:31:29 -0500

    Start Menu

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    StartMenuControls controls;
+
+    void Awake()
+    {
+        controls = new StartMenuControls();
+        controls.Enable();
+    }
+
+    void Update()
+    {
+        if (controls.StartGame.Play.WasPressedThisFrame())
+        {
+            // Interact with object
+            SceneManager.LoadScene("Main", LoadSceneMode.Additive);
+            UnityEngine.Debug.Log("Click");
+        }
+    }
+}
